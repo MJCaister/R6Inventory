72198c081c0b897430d7ea2d2bdc1bddf0db2760
NKSCPredictive <nukescrew@gmail.com>
2020-05-29 09:28:50 +1200

    Added small icons to database for operators

---
+++ db image thing.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+conn = sqlite3.connect('app.db')
+cur = conn.cursor()
+cur.execute("SELECT * FROM item WHERE type = 5;")
+results = cur.fetchall()
+for op in results:
+    print("/static/images/item/operator/small/{}.png".format(op[1].lower()))
