ea966bb92458abcb3644d3a7f064e6ae0ed62fe0
NKSCPredictive <nukescrew@gmail.com>
2020-06-10 10:33:53 +1200

    Added organisation images, started flask_login

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
 db = SQLAlchemy()
 
@@ -32,15 +34,28 @@ class OperatorOrg(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     operator_id = db.Column(db.ForeignKey('Item.id'))
     org_id = db.Column(db.ForeignKey('Item.id'))
-    
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text(32), nullable=False)
     password = db.Column(db.Text(256), nullable=False)
     image = db.Column(db.Text(64))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s{}'.format(digest, size)
 
 
 t_user_item = db.Table(
--- db image thing.py
+++ db image thing.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('app.db')
 cur = conn.cursor()
-cur.execute("SELECT * FROM item WHERE type = 5;")
+cur.execute("SELECT * FROM item WHERE type = 6;")
 results = cur.fetchall()
-for op in results:
-    print("/static/images/item/operator/large/{}.png".format(op[1].lower()))
+for item in results:
+    print("/static/images/item/organisation/small/{}.png".format(item[1].lower()))
