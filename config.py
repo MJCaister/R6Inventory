f25178956ff5342f70ddc97de62b181ed5b92a54
Matthew <nukescrew@gmail.com>
2020-07-22 08:38:55 +1200

    Whitespace

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Config(object):
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'mail.privateemail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'nukescrew@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'yurfzlksdlhysqne'
-    ADMINS = ['nukescrew@gmail.com', '16086@burnside.school.nz']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'support@r6inventory.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'Xosd1L4o!!'
+    ADMINS = ['support@r6inventory.com', 'nukescrew@gmail.com', '16086@burnside.school.nz']
