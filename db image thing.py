21a3b638685d88902557659ad8dfe20e78e2688b
NKSCPredictive <nukescrew@gmail.com>
2020-07-22 10:31:25 +1200

    Added large images for weapons

--- db image thing.py
+++ db image thing.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('app.db')
 cur = conn.cursor()
-cur.execute("SELECT * FROM item WHERE type = 6;")
+cur.execute("SELECT * FROM item WHERE type = 8;")
 results = cur.fetchall()
 for item in results:
-    print("/static/images/item/organisation/small/{}.png".format(item[1].lower()))
+    print("/static/images/item/weapon/large/{}.png".format(item[1].lower()))
